ac013d9a860a3c08138d0ebe58ced8cf8c51e5f9
wshsh <1319467412@qq.com>
2019-08-21 17:10:48 +0800

    17:10

--- src/components/content/CartContent.vue
+++ src/components/content/CartContent.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="contentcontainer">
+    
     <!-- 无商品 -->
     <div class="cart-head" v-if="!goodList.length">
       <div class="cart-title">购物车</div>
@@ -33,30 +34,24 @@
       </div>
     </div>
     <!-- 有商品 -->
-    <div class="cart-head">
+    <div class="cart-head" >
       <div class="cart-title">购物车</div>
       <div>
-        <router-link to="/user">
-          <span class="cart-ticket">领券</span>
-        </router-link>
-        <span class="cart-edit" @click="changecart">编辑</span>
+          <router-link to="/user">
+            <span class="cart-ticket">领券</span>
+          </router-link>
+          <span class="cart-edit" @click="changecart">编辑</span>
       </div>
     </div>
-    <div v-for="(item, index) in goodList" :key="index">
+    <div  v-for="(item, index) in goodList" :key="index">
       <div class="cart-item-container">
         <div class="cart-item" >
           <div class="cart-redsqure" @click="selectitem(item)"><img  v-show="(item.checked)" class="delete-good-redsqure-img" src="images/cartyes.jpg" alt=""></div>
           <div class="cart-item-body">
-            <img class="cart-item-img" :src="item.imgurl" alt />
+            <img class="cart-item-img" :src="item.imgurl" alt="">
             <div class="cart-item-right">
-              <div class="cart-item-title">
-                <span class="cart-item-special">{{item.special}}</span>
-                {{item.title}}
-              </div>
-              <div class="cart-item-price">
-                <span class="cart-item-newprice">{{item.nowprice}}</span>
-                <span class="cart-item-oldprice">{{item.oldprice}}</span>
-              </div>
+              <div class="cart-item-title"><span class="cart-item-special">{{item.special}}</span>{{item.title}}</div>
+              <div class="cart-item-price"><span class="cart-item-newprice">{{item.nowprice}}</span><span class="cart-item-oldprice">{{item.oldprice}}</span></div>
               <div class="cart-plus-minus">
                 <span class="cart-plus-minus-left" :style="{borderColor:'#999'}" @click="minus(index)">-</span>
                 <span class="cart-plus-minus-mid">{{item.count}}</span>
@@ -71,7 +66,7 @@
             <span class="delete-good-redsqure" @click="checkAll(true)"><img v-show="checkAllFlag" class="delete-good-redsqure-img" src="images/cartyes.jpg" alt=""></span>
             <span class="delete-good-choose">已选{{totalcount}}件</span>
           </div>
-          <div class="allgoods-money">合计:{{item.nowprice*item.count}}</div>
+          <div class="allgoods-money">合计:{{totalMoney}}</div>
           <router-link to="/user">
             <div class="delete-good-del">下单</div>
           </router-link>
@@ -85,11 +80,12 @@
           <div class="delete-good-del" @click="del(index)">删除所选</div>
         </div>
       </div>
+      
     </div>
   </div>
 </template>
 <script>
-import store from "../store/index.js";
+import store from "../store/index.js"
 export default {
   store:store,
   data(){
@@ -168,21 +164,6 @@ export default {
         del(index){
             this.$store.commit("del", index);
         }
-    },
-    changechoose() {
-      this.choose = !this.chooose;
-    },
-    changecart() {
-      this.look = !this.look;
-    },
-    minus(index) {
-      this.$store.commit("minus", index);
-    },
-    add(index) {
-      this.$store.commit("add", index);
-    },
-    del(index) {
-      this.$store.commit("del", index);
     }
 };
 </script>
@@ -191,16 +172,15 @@ export default {
   background: #EEEEEE;
   margin-top: 50px;
   /* height: 630px; */
-  overflow: hidden;
 }
-.cart-item {
+.cart-item{
   height: 116px;
-  position: relative;
-  margin-bottom: 8px;
+   position: relative;
+   margin-bottom: 8px;
   width: 100%;
 }
-.cart-item-title {
-  width: 240px;
+.cart-item-title{
+  width: 240px;;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -216,7 +196,7 @@ export default {
   justify-content: space-between;
   padding: 0 20px;
   line-height: 45px;
-  border-bottom: 1px solid #eeeeee;
+  border-bottom: 1px solid #EEEEEE
 }
 .cart-title {
   font-size: 20px;
@@ -286,88 +266,90 @@ export default {
   background-color: #b4282d;
   border-radius: 2px;
 }
-.cart-item {
+.cart-item{
   display: flex;
   align-items: center;
   background: white;
   height: 124px;
 }
-.cart-redsqure {
+.cart-redsqure{
   width: 20px;
   height: 20px;
   border: 1px solid #eeeeee;
   border-radius: 50%;
   margin: 10px;
 }
-.cart-item-body {
+.cart-item-body{
   padding-top: 12px;
   padding-right: 18px;
   display: flex;
 }
-.cart-item-img {
+.cart-item-img{
   width: 98px;
   height: 98px;
-  background: #eeeeee;
+  background: #EEEEEE
 }
-.cart-item-right {
+.cart-item-right{
+ 
   margin-left: 10px;
 }
-.cart-item-title {
+.cart-item-title{
   font-size: 15px;
 }
-.cart-item-price {
+.cart-item-price{
   margin-top: 60px;
   width: 78px;
   height: 16px;
 }
-.cart-item-newprice::before {
-  content: "\A5";
+.cart-item-newprice::before{
+  content: "\A5"
 }
-.cart-item-oldprice {
+.cart-item-oldprice{
   font-size: 14px;
   color: #999;
   text-decoration: line-through;
 }
-.cart-item-oldprice::before {
-  content: "\A5";
+.cart-item-oldprice::before{
+  content: "\A5"
 }
-.cart-plus-minus {
+.cart-plus-minus{
   position: absolute;
   right: 24px;
   bottom: 10px;
 }
-.cart-plus-minus-left {
+.cart-plus-minus-left{
   display: inline-block;
   margin: 0;
   padding: 0;
-  width: 33px;
-  height: 27px;
+  width: 33PX;
+  height: 27PX;
   border: 1px solid #eeeeee;
   text-align: center;
 }
-.cart-plus-minus-right {
+.cart-plus-minus-right{
   display: inline-block;
-  width: 33px;
-  height: 27px;
+  width: 33PX;
+  height: 27PX;
   margin: 0;
   padding: 0;
   border: 1px solid #eeeeee;
   text-align: center;
+  
 }
-.cart-plus-minus-mid {
+.cart-plus-minus-mid{
   display: inline-block;
-  width: 24px;
+  width: 24PX;
   margin: 0;
   padding: 0;
-  height: 27px;
+  height: 27PX;
   border-top: 1px solid #eeeeee;
   border-bottom: 1px solid #eeeeee;
   text-align: center;
 }
-.cart-item-special {
-  color: #f48f18;
+.cart-item-special{
+  color: #f48f18
 }
-.delete-good {
+.delete-good{
   background: white;
   width: 100%;
   height: 53px;
@@ -378,32 +360,32 @@ export default {
   align-items: center;
   justify-content: space-between;
 }
-.delete-good-del {
+.delete-good-del{
   width: 126px;
-  background: #b4282d;
+  background:#b4282d;
   color: white;
   text-align: center;
   height: 100%;
-  line-height: 53px;
+  line-height: 53px
 }
-.delete-good-redsqure {
+.delete-good-redsqure{
   display: inline-block;
-  width: 20px;
+   width: 20px;
   height: 20px;
   border: 1px solid #eeeeee;
   border-radius: 50%;
   margin-left: 15px;
 }
-.delete-good-choose {
+.delete-good-choose{
   font-size: 14px;
   color: #999;
 }
-.allgoods-money {
+.allgoods-money{
   color: #b4282d;
   margin-left: 40px;
   font-size: 15px;
 }
-.delete-good-redsqure-img {
+.delete-good-redsqure-img{
   width: 100%;
 }
 </style>
