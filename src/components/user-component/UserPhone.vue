f8e73204c93fba3d2f3666e9a2b197bded72a4d1
yy2209 <2209548502@qq.com>
2019-08-22 15:07:01 +0800

    yy

--- src/components/user-component/UserPhone.vue
+++ src/components/user-component/UserPhone.vue
@@ -6,18 +6,31 @@
       </div>
       <div class="user-content-login">
         <div class="user-content-number">
-          <input class="user-content-input1" type="email" placeholder="邮箱账号" v-model="message"/>
+          <input class="user-content-input1" type="number" placeholder="请输入手机号" v-model="message" />
         </div>
         <div class="user-content-number">
-          <input class="user-content-input2" type="password" placeholder="密码" v-model="message1" />
+          <input
+            class="user-content-input2"
+            type="number"
+            placeholder="请输入短信验证码"
+            v-model="message1"
+          />
+          <button class="user-content-button">获取验证码</button>
         </div>
         <div class="user-content-wrap">
-          <span class="user-content-item1">注册账号</span>
-          <span class="user-content-item2">忘记密码</span>
+          <span class="user-content-item1">遇到问题</span>
+          <span class="user-content-item2">使用密码验证登录</span>
         </div>
-        
-          <div class="user-content-go" @click="login">登录</div>
-        
+
+        <div class="user-content-go" @click="login">登录</div>
+        <div class="user-content-diquir">
+          <img src="//yanxuan.nosdn.127.net/6830e38fbcc7c038eea164ecb53365ca.png" alt />
+          <div>
+            我同意
+            <span>《服务条款》</span>和
+            <span>《网易隐私政策》</span>
+          </div>
+        </div>
       </div>
       <div class="user-content-other" @click="backtoindex">其他登录方式></div>
     </div>
@@ -29,21 +42,20 @@ export default {
   name: "usermail",
   data() {
     return {
-      message:"",
-      message1:"",
+      message: "",
+      message1: ""
     };
   },
   methods: {
     backtoindex() {
       this.$emit("sindex", "user");
     },
-    login(){
-      if(this.message&&this.message1){
-        this.$router.push("/")
-      }else{
-        alert("请输入")
+    login() {
+      if (this.message && this.message1) {
+        this.$router.push("/");
+      } else {
+        alert("请输入");
       }
-      
     }
   }
 };
@@ -76,10 +88,12 @@ export default {
 
 .user-content-login {
   padding: 0 22px;
-  margin: 96px 0 32px 0;
+  margin-bottom: 32px 0;
 }
 
 .user-content-number {
+  display: flex;
+  flex-direction: row;
   width: 100%;
   height: 52px;
 }
@@ -98,6 +112,18 @@ export default {
   border-bottom: 1px solid #c5cddb;
 }
 
+.user-content-button {
+  width: 92px;
+  height: 30px;
+  margin: 11px 0;
+  position: absolute;
+  right: 33px;
+  background: #fff;
+  border: none;
+  border: 1px solid #ccc;
+  border-radius: 2px;
+}
+
 .user-content-wrap {
   width: 100%;
   line-height: 52px;
@@ -106,6 +132,7 @@ export default {
 }
 
 .user-content-item2 {
+  color: #333;
   position: absolute;
   right: 22px;
 }
@@ -116,6 +143,27 @@ export default {
   text-align: center;
   background: #dd1a21;
   color: #fff;
+}
+
+.user-content-diquir {
+  display: flex;
+  flex-direction: row;
+  padding: 10px 0;
+}
+
+.user-content-diquir img {
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+}
+
+.user-content-diquir div {
+  font-size: 12px;
+  color: #999;
+}
+
+.user-content-diquir span {
+  color: #007aff;
 }
 
 .user-content-other {
